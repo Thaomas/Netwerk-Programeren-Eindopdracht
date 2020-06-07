d7ce76b37788bd30515b39d62ecf275005846ae9
Nicholas <nkw.leroux@student.avans.nl>
2020-06-07 20:04:40 +0200

    added listener package

---
+++ src/client/listener/ChatListener.java
@@ -0,0 +1,52 @@
+package client.listener;
+
+import client.ChatGUI;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ChatListener implements Runnable {
+    private final ChatGUI chatGUI;
+    private final Socket socket;
+    private final String roomCode;
+
+    public ChatListener(ChatGUI gui, Socket socket, String roomCode) {
+        this.chatGUI = gui;
+        this.socket = socket;
+        this.roomCode = roomCode;
+    }
+
+    /**
+     * Method used to listen to the incoming messages from the server. Only used for the chat "global chat".
+     */
+    @Override
+    public void run() {
+        boolean connected = true;
+        DataInputStream in = null;
+        try {
+            in = new DataInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (connected) {
+            try {
+                String input = in.readUTF();
+                String command = input.substring(0, 4);
+                if (command.equals("Disc")) {
+                    connected = false;
+                    continue;
+                }
+                String roomCode = input.substring(4, 8);
+                if (roomCode.equals(this.roomCode)) {
+                    if (command.equals("CMes")) {
+                        chatGUI.addMessage(input.substring(8));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                connected = false;
+            }
+        }
+    }
+}
---
+++ src/client/listener/GameListener.java
@@ -0,0 +1,77 @@
+package client.listener;
+
+import client.GameGUI;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class GameListener implements Runnable {
+    private final GameGUI gameGui;
+    private final Socket socket;
+    private final String roomCode;
+
+    public GameListener(GameGUI gameGUI, Socket socket, String roomCode) {
+        this.gameGui = gameGUI;
+        this.socket = socket;
+        this.roomCode = roomCode;
+    }
+
+    /**
+     * Method used to listen to the incoming messages from the server. Used for all game rooms.
+     */
+    @Override
+    public void run() {
+        boolean connected = true;
+        DataInputStream in = null;
+        try {
+            in = new DataInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (connected) {
+            try {
+                String input = in.readUTF();
+                if (input.equals("Disc")) {
+                    connected = false;
+                    continue;
+                }
+                String command = input.substring(0, 4);
+                String roomCode = input.substring(4, 8);
+                if (command.equals("CMes")) {
+                    if (roomCode.equals(this.roomCode))
+                        gameGui.messageToGameChat(input.substring(8));
+                    else if (roomCode.equals("main"))
+                        gameGui.messageToMainChat(input.substring(8));
+                } else if (command.equals("GMes")) {
+                    if (roomCode.equals(this.roomCode)) {
+                        if (input.substring(8).contains("Conn")) {
+                            gameGui.setOpponentName(input.substring(12));
+                        } else {
+                            switch (input.substring(8)) {
+                                case "Move":
+                                    gameGui.placeDisc();
+                                    break;
+                                case "Win":
+                                    gameGui.restartGame(input.substring(8));
+                                    break;
+                                case "Lose":
+                                    gameGui.restartGame(input.substring(8));
+                                    break;
+                                case "Vote":
+                                    gameGui.vote();
+                                    break;
+                                case "Restart":
+                                    gameGui.restart();
+                                    break;
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                connected = false;
+            }
+        }
+    }
+}
